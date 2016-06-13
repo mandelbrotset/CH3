0e9ed8bf7ff11af1c809201bf656a5274b73d1a9
Isak Eriksson <isak.eriksson@mail.com>
2016-06-13 21:29:00 +0200

    Added missing files..

---
+++ CH3/Camera/ThirdPerson.cs
@@ -0,0 +1,15 @@
+﻿using OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH3.Camera
+{
+    abstract class ThirdPerson : Camera
+    {
+        public GameObject follow { get; set; }
+        public abstract void UpdateCamera();
+    }
+}
