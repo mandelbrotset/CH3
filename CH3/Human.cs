e82ef30be430cb0e99ee7bcef7cbb843a6e01015
Isak Eriksson <isak.eriksson@mail.com>
2016-06-14 21:12:28 +0200

    Moved things from Player to Human

--- CH3/Human.cs
+++ CH3/Human.cs
@@ -8,8 +8,45 @@ namespace CH3
 {
     public class Human : Player
     {
+        private Dictionary<byte, GameObject.Input> controls;
         public Human(Vector3 position, Vector3 scale, float rotation, BasicShaderProgram shader, NormalShader normalShader, CelShader celShader, DepthShader depthShader) : base(position, scale, rotation, shader, normalShader, celShader, depthShader)
         {
+            SetControls();
+            CH3.Input.SubscribeKeyDown(KeyDown);
+            CH3.Input.SubscribeKeyUp(KeyUp);
+            CH3.Input.SubscribeMouseMovement(MouseMovement);
+        }
+
+        private void MouseMovement(float pitch, float yaw)
+        {
+            rotationZ = -yaw;
+        }
+
+        private void SetControls()
+        {
+            controls = new Dictionary<byte, Input>();
+            controls.Add(119, GameObject.Input.FORWARD);//w
+            controls.Add(115, GameObject.Input.BACKWARD);//s
+            controls.Add(97, GameObject.Input.LEFT);//a
+            controls.Add(100, GameObject.Input.RIGHT);//d
+        }
+
+        private void KeyDown(byte key, int x, int y)
+        {
+            GameObject.Input input;
+            if (controls.TryGetValue(key, out input))
+            {
+                ActivateInput(input);
+            }
+        }
+
+        private void KeyUp(byte key, int x, int y)
+        {
+            GameObject.Input input;
+            if (controls.TryGetValue(key, out input))
+            {
+                DeactivateInput(input);
+            }
         }
     }
 }
--- CH3/Player.cs
+++ CH3/Player.cs
@@ -8,7 +8,6 @@ namespace CH3
 {
     public class Player : DynamicObject
     {
-        private Dictionary<byte, GameObject.Input> controls;
 
         public Player(Vector3 position, Vector3 scale, float rotation, BasicShaderProgram shader, NormalShader normalShader, CelShader celShader, DepthShader depthShader) : base(position, scale, rotation, shader, normalShader, celShader, depthShader)
         {
@@ -16,42 +15,7 @@ namespace CH3
             inputs.Add(Input.BACKWARD, false);
             inputs.Add(Input.LEFT, false);
             inputs.Add(Input.RIGHT, false);
-            controls = new Dictionary<byte, Input>();
-            SetControls();
-            CH3.Input.SubscribeKeyDown(KeyDown);
-            CH3.Input.SubscribeKeyUp(KeyUp);
-            CH3.Input.SubscribeMouseMovement(MouseMovement);
-            LoadModel("../../models/house.obj", "../../textures/house.png", 1);
-        }
-
-        private void MouseMovement(float pitch, float yaw)
-        {
-            rotationZ = -yaw;
-        }
-
-        private void SetControls()
-        {
-            controls.Add(119, GameObject.Input.FORWARD);//w
-            controls.Add(115, GameObject.Input.BACKWARD);//s
-            controls.Add(97, GameObject.Input.LEFT);//a
-            controls.Add(100, GameObject.Input.RIGHT);//d
-        }
-        private void KeyDown(byte key, int x, int y)
-        {
-            GameObject.Input input;
-            if (controls.TryGetValue(key, out input))
-            {
-                ActivateInput(input);
-            }
-        }
-
-        private void KeyUp(byte key, int x, int y)
-        {
-            GameObject.Input input;
-            if (controls.TryGetValue(key, out input))
-            {
-                DeactivateInput(input);
-            }
+            LoadModel("../../models/house.obj", "../../textures/house.png", 1);//borde ändra detta sen
         }
 
         protected override void Act(Input input)
@@ -77,15 +41,14 @@ namespace CH3
             Vector3 translation = new Vector3(x, y, 0);
             translation *= Matrix4.CreateRotationZ(-rotationZ);
             velocity = translation;
-            //Console.WriteLine(translation + " : " + position);
         }
 
-        public void ActivateInput(Input input)
+        protected void ActivateInput(Input input)
         {
             HandleActivate(input);
         }
 
-        public void DeactivateInput(Input input)
+        protected void DeactivateInput(Input input)
         {
             HandleDeactivate(input);
         }
